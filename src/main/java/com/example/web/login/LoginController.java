a892ccfa6a3d70573f502be26b285d462e6c71b2
Francisco Puma <fppuma@gmail.com>
2023-04-07 13:02:56 -0500

    login: errorMessage

--- src/main/java/com/example/web/login/LoginController.java
+++ src/main/java/com/example/web/login/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.web.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,11 +20,12 @@ public class LoginController {
 
   @PostMapping("login")
   public String processLogin(@RequestParam String username,
-    @RequestParam String password) {
+    @RequestParam String password,
+    ModelMap model) {
     if (this.authenticateService.authenticate(username, password)){
       return "welcome";
-
     }
+    model.put("errorMessage", "Invalid Credentials");
     return "login";
   }
 }
